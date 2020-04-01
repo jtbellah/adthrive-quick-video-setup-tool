7750ff2a9d6713c80f7fc74dfeedda9052fb1958
forbesgillikin <36290669+forbesgillikin@users.noreply.github.com>
2020-04-01 18:21:46 -0500

    v2.1 changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,60 @@
-import React from "react";
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/label-has-for */
+import React from 'react';
 
 function Form(props) {
+  const {
+    siteName,
+    setSiteName,
+    apiKey,
+    setApiKey,
+    apiSecret,
+    setApiSecret,
+    handleForm,
+    handleFormReset,
+    sitemapUrl,
+    setSitemapUrl,
+    error,
+    playersExist,
+    playlistsExist,
+    videosExist,
+    loading,
+  } = props;
+
+  if (loading) {
+    return (
+      <div className="col-5">
+        <div className="d-flex justify-content-center">
+          <div
+            className="spinner-border text-primary"
+            style={{ width: '3rem', height: '3rem' }}
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <form className="col-5">
       {/* site name field */}
+      {playersExist && (
+        <div className="alert alert-warning">
+          ⚠️ Players for this property already exist.
+        </div>
+      )}
+      {playlistsExist && (
+        <div className="alert alert-warning">
+          ⚠️ Playlists for this property already exist.
+        </div>
+      )}
+      {videosExist && (
+        <div className="alert alert-warning">
+          ⚠️ Videos for this property already exist.
+        </div>
+      )}
       <div className="form-group">
         <label htmlFor="siteNameInput" className="font-weight-bold">
           Site Name
@@ -12,8 +63,8 @@ function Form(props) {
           type="text"
           className="form-control"
           id="siteNameInput"
-          value={props.siteName}
-          onChange={e => props.setSiteName(e.target.value)}
+          value={siteName}
+          onChange={e => setSiteName(e.target.value)}
           placeholder="Site Name"
         ></input>
       </div>
@@ -27,8 +78,8 @@ function Form(props) {
           type="text"
           className="form-control"
           id="apiKeyInput"
-          value={props.apiKey}
-          onChange={e => props.setApiKey(e.target.value)}
+          value={apiKey}
+          onChange={e => setApiKey(e.target.value)}
           placeholder="API Key"
         ></input>
       </div>
@@ -42,10 +93,28 @@ function Form(props) {
           type="text"
           className="form-control"
           id="apiSecretInput"
-          value={props.apiSecret}
-          onChange={e => props.setApiSecret(e.target.value)}
+          value={apiSecret}
+          onChange={e => setApiSecret(e.target.value)}
           placeholder="API Secret"
         ></input>
+      </div>
+
+      {/* sitemap url field */}
+      <div className="form-group">
+        <label htmlFor="sitemapUrlInput" className="font-weight-bold">
+          Mediavine Sitemap Url
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="sitemapUrlInput"
+          value={sitemapUrl}
+          onChange={e => setSitemapUrl(e.target.value)}
+          placeholder="Sitemap Url"
+        ></input>
+        {error && (
+          <div className="alert alert-danger mt-1">❗Response or XML Error</div>
+        )}
       </div>
 
       {/* submit btn */}
@@ -54,8 +123,8 @@ function Form(props) {
         <button
           type="button"
           className="btn btn-primary mt-3"
-          onClick={props.handleForm}
-          disabled={!props.siteName || !props.apiKey || !props.apiSecret}
+          onClick={handleForm}
+          // disabled={!siteName || !apiKey || !apiSecret}
         >
           Submit
         </button>
@@ -64,8 +133,7 @@ function Form(props) {
         <button
           type="button"
           className="btn btn-danger mt-3 ml-3"
-          onClick={props.handleFormReset}
-          disabled={!props.siteName || !props.apiKey || !props.apiSecret}
+          onClick={handleFormReset}
         >
           Reset Form
         </button>
---
+++ src/lib/import/bulkImport.js
@@ -0,0 +1,30 @@
+import wait from 'waait';
+
+// Where the magic happens
+const bulkImport = async (
+  jwApiInstance,
+  videos,
+  setImportTotal,
+  setImporting
+) => {
+  let count = 0;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const video of videos) {
+    const shouldWait = Number.isInteger((count + 1) / 60);
+
+    if (shouldWait) {
+      console.log('Waiting for rate limit reset...');
+      await wait(70000);
+    }
+
+    jwApiInstance.videos.create(video);
+
+    count += 1;
+  }
+
+  setImporting(false);
+  setImportTotal(count);
+};
+
+export default bulkImport;
